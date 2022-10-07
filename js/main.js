3d3e580c0c225b07472dcb57ef0bab9c21226b05
Julian Riedinger <julianriedinger27@gmail.com>
2022-10-06 21:06:25 -0300

    se agrego seccion productos con cards

--- js/main.js
+++ js/main.js
@@ -1,51 +1,55 @@
-const header = document.querySelector('header')
-const nav = document.querySelector('header nav')
-const hamburger = document.querySelector('.hamburger')
-const logo = document.querySelector('.logo')
-const close = document.querySelector('.cerrar')
-const mobileMenu = document.querySelector('.mobile-menu')
-const itemsCromatografos = document.querySelector('.dropdown-items-cromatografos ul')
-const downArrows = document.querySelectorAll('.down-arrow')
-const upArrows = document.querySelectorAll('.up-arrow')
-
+const header = document.querySelector('header');
+const nav = document.querySelector('header nav');
+const hamburger = document.querySelector('.hamburger');
+const logo = document.querySelector('.logo');
+const close = document.querySelector('.cerrar');
+const mobileMenu = document.querySelector('.mobile-menu');
+const itemsCromatografos = document.querySelector(
+	'.dropdown-items-cromatografos ul'
+);
+const downArrows = document.querySelectorAll('.down-arrow');
+const upArrows = document.querySelectorAll('.up-arrow');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.add('d-none')
-  close.classList.remove('d-none')
-  mobileMenu.classList.remove('d-none')
-  logo.classList.add('d-none')
-  header.classList.add('h-auto')
-  nav.classList.add('d-block')
-})
+	hamburger.classList.add('d-none');
+	close.classList.remove('d-none');
+	mobileMenu.classList.remove('d-none');
+	logo.classList.add('d-none');
+	header.classList.add('h-auto');
+	nav.classList.add('d-block');
+});
 
 close.addEventListener('click', () => {
-  close.classList.add('d-none')
-  hamburger.classList.remove('d-none')
-  logo.classList.remove('d-none')
-  mobileMenu.classList.add('d-none')
-  header.classList.remove('h-auto')
-  nav.classList.remove('d-block')
-})
+	close.classList.add('d-none');
+	hamburger.classList.remove('d-none');
+	logo.classList.remove('d-none');
+	mobileMenu.classList.add('d-none');
+	header.classList.remove('h-auto');
+	nav.classList.remove('d-block');
+});
 
+/* downArrows.forEach((arrow) =>
+	arrow.addEventListener('click', (evt) => {
+		evt.target.classList.add('d-none');
+		evt.target.parentElement.nextElementSibling.classList.remove('d-none');
+		evt.target.nextElementSibling.classList.remove('d-none');
+	})
+);
 
-downArrows.forEach(arrow => arrow.addEventListener('click', (evt) => {
-  evt.target.classList.add('d-none')
-  evt.target.parentElement.nextElementSibling.classList.remove('d-none')
-  evt.target.nextElementSibling.classList.remove('d-none')
-}))
+upArrows.forEach((arrow) =>
+	arrow.addEventListener('click', (evt) => {
+		evt.target.classList.add('d-none');
+		evt.target.parentElement.nextElementSibling.classList.add('d-none');
+		evt.target.previousElementSibling.classList.remove('d-none');
+	})
+);
 
-upArrows.forEach(arrow => arrow.addEventListener('click', (evt) => {
-  evt.target.classList.add('d-none')
-  evt.target.parentElement.nextElementSibling.classList.add('d-none')
-  evt.target.previousElementSibling.classList.remove('d-none')
-}))
-
-
-
-const cromatografosHTML = cromatografos.map((item,index) => {
-  return `
+const cromatografosHTML = cromatografos
+	.map((item, index) => {
+		return `
     <li class='bullet' data-index='${index}'><a href=''>${item.nombre}</a></li>
-  `
-}).join('')
+  `;
+	})
+	.join('');
 
-itemsCromatografos.innerHTML = cromatografosHTML
+itemsCromatografos.innerHTML = cromatografosHTML; */
--- js/productos.js
+++ js/productos.js
@@ -3,7 +3,7 @@ const cromatografos = [
 		nombre: 'C6+',
 		descripcion:
 			'Aplicaciones Ideal para aplicaciones de C6+, incluidas: Transferencia de custodia de gas natural, medición de gas de producción, generación de energía, control del gas combustible ',
-		img: './images/c6.png',
+		img: '../images/c6.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/brochure-rosemount-gas-chromatograph-solutions-for-natural-gas-transmission-applications-en-72744.pdf',
 	},
@@ -11,7 +11,7 @@ const cromatografos = [
 		nombre: 'C9+',
 		descripcion:
 			'Aplicaciones Ideal para C9+ y aplicaciones personalizadas, incluidas: Refinerías, Petroquímica, Control del gas combustible, Procesamiento de gas, Generación de energía, Monitoreo ambiental. El 700XA es un cromatógrafo de gases con horno sin aire, completamente funcional que le permite utilizar las aplicaciones de análisis extendido en una carcasa resistente montable en campo. La aplicación del 700XA C9+ incluye los cálculos del HCDP que pueden determinar este punto en cuatro presiones diferentes y el cricondentherm (el HCDP más alto a cualquier presión).',
-		img: './images/c9.png',
+		img: '../images/c9.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/brochure-rosemount-gas-chromatograph-solutions-for-natural-gas-transmission-applications-en-72744.pdf ',
 	},
@@ -19,7 +19,7 @@ const cromatografos = [
 		nombre: 'C6+ H2S',
 		descripcion:
 			'Calidad del gas: además de la medición de energía estándar, es posible que, por exigencias contractuales o de salud y seguridad, deba brindar mediciones adicionales. Estas mediciones incluirán los componentes sulfurosos del gas del proceso. En general, los componentes sulfurosos de interés se definen como H2 S, COS (con frecuencia se informan en forma conjunta como H2 S) y el total de todos los componentes sulfurosos que se miden en forma individual o colectiva como el sulfuro total. Emerson puede brindan todas estas mediciones en la plataforma XA, por lo que se reduce la complejidad del sistema de medición de la calidad del gas. ',
-		img: './images/c6h2s.png',
+		img: '../images/c6h2s.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/brochure-rosemount-gas-chromatograph-solutions-for-natural-gas-transmission-applications-en-72744.pdf',
 	},
@@ -30,7 +30,7 @@ const productos = [
 		nombre: 'C6+',
 		descripcion:
 			'Aplicaciones Ideal para aplicaciones de C6+, incluidas: Transferencia de custodia de gas natural, medición de gas de producción, generación de energía, control del gas combustible ',
-		img: './images/c6.png',
+		img: '../images/c6.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/brochure-rosemount-gas-chromatograph-solutions-for-natural-gas-transmission-applications-en-72744.pdf',
 	},
@@ -38,7 +38,7 @@ const productos = [
 		nombre: 'C9+',
 		descripcion:
 			'Aplicaciones Ideal para C9+ y aplicaciones personalizadas, incluidas: Refinerías, Petroquímica, Control del gas combustible, Procesamiento de gas, Generación de energía, Monitoreo ambiental. El 700XA es un cromatógrafo de gases con horno sin aire, completamente funcional que le permite utilizar las aplicaciones de análisis extendido en una carcasa resistente montable en campo. La aplicación del 700XA C9+ incluye los cálculos del HCDP que pueden determinar este punto en cuatro presiones diferentes y el cricondentherm (el HCDP más alto a cualquier presión).',
-		img: './images/c9.png',
+		img: '../images/c9.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/brochure-rosemount-gas-chromatograph-solutions-for-natural-gas-transmission-applications-en-72744.pdf ',
 	},
@@ -46,7 +46,7 @@ const productos = [
 		nombre: 'C6+ H2S',
 		descripcion:
 			'Calidad del gas: además de la medición de energía estándar, es posible que, por exigencias contractuales o de salud y seguridad, deba brindar mediciones adicionales. Estas mediciones incluirán los componentes sulfurosos del gas del proceso. En general, los componentes sulfurosos de interés se definen como H2 S, COS (con frecuencia se informan en forma conjunta como H2 S) y el total de todos los componentes sulfurosos que se miden en forma individual o colectiva como el sulfuro total. Emerson puede brindan todas estas mediciones en la plataforma XA, por lo que se reduce la complejidad del sistema de medición de la calidad del gas. ',
-		img: './images/c6h2s.png',
+		img: '../images/c6h2s.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/brochure-rosemount-gas-chromatograph-solutions-for-natural-gas-transmission-applications-en-72744.pdf',
 	},
@@ -54,7 +54,7 @@ const productos = [
 		nombre: 'FB1100 & FB1200',
 		descripcion:
 			'El computador rentable de caudal antideflagrante FB1100 mide y monitoriza el caudal de gas para un solo punto de medición de presión diferencial. Además de proporcionar un nuevo nivel de confianza en las mediciones, este dispositivo de baja potencia puede funcionar independientemente sin alimentación externa, durante un año como máximo, lo que convierte al modelo FB1100 en un reemplazo ideal para registradores de gráficos. Este dispositivo fácil de configurar y fácil de usar está diseñado para aplicaciones de medición donde no se requiere control de caudal. Sin embargo, tiene la capacidad de usar una sola salida de pulso (DO) para dirigir un odorizador. El computador de caudal FB1100 es parte de la plataforma de medición y control de la serie FB de última generación de Emerson que ofrece un conjunto común de herramientas de configuración, incluyendo FBxConnect™, para acelerar la configuración y facilitar las tareas que se realizan comúnmente.',
-		img: './images/fb1100.png',
+		img: '../images/fb1100.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/hoja-de-datos-del-producto-fb1100-flow-computer-spanish-ras-es-4025670.pdf',
 	},
@@ -62,7 +62,7 @@ const productos = [
 		nombre: 'FB2100 & FB2200',
 		descripcion:
 			'El computador de caudal FB2100 es un dispositivo rentable, de baja potencia, de montaje en campo que mide y monitoriza el caudal de gases y líquidos para un punto de medición lineal o de presión diferencial. Está diseñado para una configuración simple y facilidad de uso y es una solución ideal para aplicaciones de medición donde no se requiere control. El modelo FB2100 proporciona un registro de auditoría completo, incluyendo históricos mejorados, registros de alarmas y eventos. Las opciones de E/S pueden ser tan simples como una sola salida discreta (DO) para impulsar un odorizador o pueden expandirse para dar dos canales analógicos (AI o AO) configurables por el usuario y dos canales discretos (DI, DO o PI) configurables por el usuario. Para garantizar la seguridad, el modelo FB2100 también puede equiparse con FBxWifi™ opcional que permite al personal de campo configurar la computadora de caudal y recuperar datos desde fuera del área clasificada. El computador de caudal FB2200 ofrece una mayor flexibilidad, en comparación con nuestro modelo FB2100, con la capacidad de medir y controlar el caudal de gases y líquidos hasta para dos puntos de medición lineales o de presión diferencial.',
-		img: './images/fb2100.png',
+		img: '../images/fb2100.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/fb2100-flow-computer-spanish-es-4025666.pdf',
 	},
@@ -70,8 +70,70 @@ const productos = [
 		nombre: 'FB3000',
 		descripcion:
 			'El establecimiento de una infraestructura de automatización segura y estandarizada para compartir datos y diagnósticos en toda la empresa impulsará mejoras operativas continuas. Por eso, hemos diseñado la RTU FB3000 de última generación con el hardware, el software y las aplicaciones necesarias para impulsar la transformación digital mientras se encuentra en funcionamiento. Desde su forma y funcionalidad superiores hasta la capacidad de configuración y programación líderes en la industria, la RTU FB3000 es ágil y evolucionará con facilidad junto con los requisitos cambiantes de su sitio.',
-		img: './images/fb3000.png',
+		img: '../images/fb3000.png',
 		catalogo:
 			'https://www.emerson.com/documents/automation/fb3000-rtu-for-water-wastewater-industries-en-7562128.pdf',
 	},
 ];
+
+const productsContainer = document.querySelector(
+	'.productos .products-container'
+);
+const productDescription = document.querySelector(
+	'.productos .product-description'
+);
+
+const addProducts = () => {
+	productsContainer.innerHTML += productos
+		.map((producto, index) => {
+			return `
+    <div class='product-card'>
+      <img src='${producto.img}' alt='${producto.nombre}'/>
+      <div class='product-card-content'>
+        <h3>${producto.nombre}</h3>
+        <button data-index='${index}'>Conocer mas</button>
+      </div>
+    </div>
+  `;
+		})
+		.join('');
+
+	document.querySelectorAll('.product-card button').forEach((button) =>
+		button.addEventListener('click', (evt) => {
+			showProductDescription(evt.target.dataset.index);
+		})
+	);
+};
+
+const showProductDescription = (index) => {
+	const product = productos[index];
+
+	productsContainer.classList.add('d-none');
+	productDescription.innerHTML = `
+    <div>
+      <img src='../images/back-arrow.png' alt='back-arrow' class='description-arrow'>
+      <div class='product'>
+        <div>
+          <h2>${product.nombre}</h2>
+          <img src='${product.img}' alt='${product.nombre}' />
+        </div>
+  
+        <div>
+          <p class='short'>${product.descripcion}</p>
+        </div>
+      </div>
+			<h3>Catalogo:</h3>
+			<p>Haz click <a href='${product.catalogo}' target='_BLANK'>Aqui</a> para obtener mas informacion acerca de ${product.nombre}</p>
+    </div>
+  `;
+
+	productDescription.classList.remove('d-none');
+	document
+		.querySelector('.product-description .description-arrow')
+		.addEventListener('click', () => {
+			productDescription.classList.add('d-none');
+			productsContainer.classList.remove('d-none');
+		});
+};
+
+addProducts();
